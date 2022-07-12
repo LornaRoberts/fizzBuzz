ec60adc2ace8690903b7743fa2a355fb7d05f1b2
Lorna <lornaroberts99@gmail.com>
2022-07-12 20:43:08 +0100

    actual fizzbuzz programme uploaded

---
+++ fizzbuzz.js
@@ -0,0 +1,22 @@
+/* CHALLENGE- Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions.
+For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3),
+print "Buzz" instead */
+
+function fizzBuzz(){
+
+for (let i = 1; i <= 100; i++){
+
+if (i % 3 === 0 && i % 5 === 0){
+ console.log("Fizzbuzz");
+}else if (i % 3 === 0){
+    console.log("Fizz");
+}else if (i % 5 === 0){
+    console.log("Buzz");
+}else{
+    console.log(i);
+}
+
+}
+};
+
+fizzBuzz();
